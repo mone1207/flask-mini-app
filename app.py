8ddf1d61848004c4c14333279f5f2fcf515e4ead
mone1207 <mone1207@outlook.jp>
2025-08-22 13:29:41 +0900

    add.py編集練習

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, Flask!"
 
+@app.route("/about")
+def about():
+    return "これはAboutページです"
+
 if __name__ == "__main__":
     app.run(debug=True)
